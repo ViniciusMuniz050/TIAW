246f9f96cf0a8868620503ddbcc51abaf46d2651
MarcusVini810 <marcus.vinicius@sga.pucminas.br>
2025-06-01 15:47:50 -0300

    Atualização do JS do Sprint1

--- src/sprint_1/muda_perfil.js
+++ src/sprint_1/muda_perfil.js
@@ -6,21 +6,24 @@ const dados = {
         login: "admin",
         senha: "123",
         nome: "Administrador do Sistema",
-        email: "admin@abc.com"
+        email: "admin@abc.com",
+        foto: "img/perfil.png"
       },
       {
         id: 2,
         login: "user",
         senha: "123",
         nome: "Usuario Comum",
-        email: "user@abc.com"
+        email: "user@abc.com",
+        foto: "img/perfil.png"
       },
       {
         id: 3,
         login: "rommel",
         senha: "123",
         nome: "Rommel",
-        email: "rommel@gmail.com"
+        email: "rommel@gmail.com",
+        foto: "img/perfil.png"
       }
     ]
   };
@@ -35,6 +38,7 @@ const dados = {
       document.getElementById("nome").value = usuario.nome;
       document.getElementById("email").value = usuario.email;
       document.getElementById("senha").value = usuario.senha;
+      document.getElementById("inputFoto").value = usuario.foto;
     }
   };
   
@@ -43,12 +47,14 @@ const dados = {
     const nome = document.getElementById("nome").value;
     const email = document.getElementById("email").value;
     const senha = document.getElementById("senha").value;
+    const foto = document.getElementById("inputFoto").value;
   
     const usuario = dados.usuarios.find(u => u.id === usuarioLogadoId);
     if (usuario) {
       usuario.nome = nome;
       usuario.email = email;
       usuario.senha = senha;
+      usuario.foto = foto;
   
       console.log("Usuário atualizado:", usuario);
       alert("Perfil atualizado com sucesso!");
@@ -65,7 +71,8 @@ const dados = {
     if (file) {
       const reader = new FileReader();
       reader.onload = function(e) {
-        document.getElementById("fotoPerfil").src = e.target.result;
+        document.getElementById("fotoPerfilHeader").src = e.target.result;
+        document.getElementById("fotoPerfilMain").src = e.target.result;
       };
       reader.readAsDataURL(file);
     }
